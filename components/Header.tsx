a7ab56ad51e16fff2364a33703aecca0b3720322
David Cerniglia <dcerniglia@thedevshop.com>
2022-10-09 19:31:15 -0400

    unroate nav items

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,16 +6,16 @@ const Header = () => {
     <nav className="h-18 w-full p-4 pt-5 fixed backdrop-blur-lg">
       <ul className="flex items-center justify-around w-full font-sm-heading text-xl font-semibold">
         {/* {TODO: change these to link elements} */}
-        <li className="inline -rotate-12 active:rotate-0">
+        <li className="inline">
           <Link href={"#about"}>about</Link>
         </li>
-        <li className="inline -rotate-12">
+        <li className="inline">
           <Link href={"#work"}>work</Link>
         </li>
-        <li className="inline -rotate-12">
+        <li className="inline">
           <Link href={"#play"}>play</Link>
         </li>
-        <li className="inline -rotate-12">
+        <li className="inline">
           <Link href={"#build"}>build</Link>
         </li>
       </ul>
