dca7e312db782887c138af5d0525f9d51128c664
David Cerniglia <dcerniglia@thedevshop.com>
2022-10-09 19:17:23 -0400

    set scroll margin on content section

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -7,9 +7,9 @@ interface ContentSectionProps {
 
 function ContentSection(props: ContentSectionProps) {
   return (
-    <div className="w-100">
-				 <h2 className="font-sm-heading font-bold text-xl pt-6 pb-3 sticky top-10 bg-background">{props.title}</h2>
-      <p className="mb-8">{props.body}</p>
+    <div className="w-100 mt-10 scroll-mt-16" id={props.title}>
+				 <h2 className="font-sm-heading font-bold text-xl pt-6 pb-3 sticky top-10 bg-background" >{props.title}</h2>
+      <p className="" >{props.body}</p>
     </div>
   );
 }
